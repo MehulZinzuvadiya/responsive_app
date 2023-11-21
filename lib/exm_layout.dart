44ca45a3ad74475d40a517e4512c3ce76fd37129
stackApp <mehul.stackapp@gmail.com>
2023-11-21 16:04:45 +0530

    ghg

--- lib/exm_layout.dart
+++ lib/exm_layout.dart
@@ -1,1 +1,30 @@
+import 'package:flutter/material.dart';
 
+class LayoutScreen extends StatefulWidget {
+  const LayoutScreen({super.key});
+
+  @override
+  State<LayoutScreen> createState() => _LayoutScreenState();
+}
+
+class _LayoutScreenState extends State<LayoutScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        title: Text('Layout Builder Demo'),
+        centerTitle: true,
+      ),
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          if (constraints.maxWidth < 600) {
+            return Center(child: Text('Small Screen'));
+          } else {
+            return Center(child: Text('Large Screen'));
+          }
+        },
+      ),
+    ));
+  }
+}
